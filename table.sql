83b0000c5b575b264d0d8c89b1a09df4c1c2a569
Bekjon Makhmudov <makhmudov_b@auca.kg>
2021-12-15 22:08:41 +0600

    not null

--- table.sql
+++ table.sql
@@ -1,39 +1,39 @@
 CREATE TABLE dbo.SubscriptionTypes
 (
-    ID INT,
-    [NAME] NVARCHAR(32),
-    Description NVARCHAR(128),
+    ID INT NOT NULL,
+    [NAME] NVARCHAR(32) NOT NULL,
+    Description NVARCHAR(128) NOT NULL,
     PRIMARY Key (ID)
 )
 CREATE TABLE dbo.Countries
 (
-    ID INT PRIMARY KEY,
-    Name NVARCHAR(64)
+    ID INT PRIMARY KEY NOT NULL,
+    Name NVARCHAR(64) NOT NULL
 )
 CREATE TABLE dbo.Users
 (
-  UserID INT PRIMARY KEY,
+  UserID INT PRIMARY KEY NOT NULL,
   Preferences BIT,
-  FirstName NVARCHAR(64),
-  SecondName NVARCHAR(64),
-  Email NVARCHAR(320),
-  Gender BIT,
-  BirthdayDate DATE,
-  CountryID INT,
+  FirstName NVARCHAR(64) NOT NULL,
+  SecondName NVARCHAR(64) NOT NULL,
+  Email NVARCHAR(320) NOT NULL,
+  Gender BIT NOT NULL,
+  BirthdayDate DATE NOT NULL,
+  CountryID INT NOT NULL,
   FOREIGN KEY (CountryID) REFERENCES dbo.Countries(ID),
   RegistrationDate DATE,
-  ProneNumber NCHAR(12),
-  Address NVARCHAR(72),
-  UserInterfaceLanguage INT
+  ProneNumber NCHAR(12) NOT NULL,
+  Address NVARCHAR(72) NOT NULL,
+  UserInterfaceLanguage INT NOT NULL
 )
 CREATE TABLE dbo.UserSubscription
 (
-    ID INT PRIMARY Key,
-    Email NVARCHAR(320),
-    LastPurchaseDate DATE,
-    SubscriptionTypesID INT,
+    ID INT PRIMARY Key NOT NULL,
+    Email NVARCHAR(320) NOT NULL,
+    LastPurchaseDate DATE NOT NULL,
+    SubscriptionTypesID INT NOT NULL,
     FOREIGN KEY (SubscriptionTypesID) REFERENCES dbo.SubscriptionTypes(ID),
-    PaymentsID INT,
-    UserID INT,
+    PaymentsID INT NOT NULL,
+    UserID INT NOT NULL,
     FOREIGN KEY (UserID) REFERENCES dbo.Users(UserID)
 )
